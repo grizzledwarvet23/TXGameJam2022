248bc02cbf51ca6975eb7129ac56f98e1d5a4281
Kumatamo-AI <atilla.istami@utexas.edu>
2022-09-24 10:12:30 -0500

    sudocode for blue and yellow powers

---
+++ Assets/Scripts/Powers.cs
@@ -0,0 +1,23 @@
+
+
+/*
+
+yellowPower(double speed) {
+    jump and movement is disabled
+    speed is stored from last velocity before landing
+
+    arrow keys determine vector of movement
+
+    when press button, player launches in the direction of the vector  
+
+}
+
+
+bluePower() {
+    if move, speed increases by *** per *** distance
+    jump conserves momentum
+    if land, speed will decrease by *** until 0
+}
+
+
+*/
