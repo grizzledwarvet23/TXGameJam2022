d56054e9ccb422c889e01239f92a095a89aa9c41
Mizichi <jbc3584@utexas.edu>
2022-09-25 19:34:05 -0500

    F

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject credits;
    public void PlayButton ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,5 +15,15 @@ public class MainMenu : MonoBehaviour
     public void ExitButton ()
     {
         Application.Quit();
+    }
+
+    public void CreditsButton()
+    {
+        credits.SetActive(true);
+    }
+
+    public void Back()
+    {
+        credits.SetActive(false);
     }
 }
